c3f27eb0d24d15293ae379dc940bfbcb5468fb1e
Amber L Lowe <52145666+amberlowe1001@users.noreply.github.com>
2020-09-26 22:36:16 -0700

    first commit

--- python_4_evryone_8_5/mbox-short.txt/__pycache__/mbox-short.txt/email.py
+++ python_4_evryone_8_5/mbox-short.txt/__pycache__/mbox-short.txt/email.py
@@ -9,26 +9,26 @@ cur.execute('''
 CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
 fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'mbox-short.txt'
+if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1]
-    # print(email)
+    print(email)
     org = email.split('@')[1]
-    # print(org)
-    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    print(org)
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (email,))
     row = cur.fetchone()
     if row is None:
         cur.execute('''INSERT INTO Counts (org, count)
-                VALUES (?, 1)''', (org,))
+                VALUES (?, 1)''', (email,))
     else:
         cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
                     (org,))
 
     conn.commit()
-# https://www.sqlite.org/lang_select.html
+#https://www.sqlite.org/lang_select.html
 sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 for row in cur.execute(sqlstr):
